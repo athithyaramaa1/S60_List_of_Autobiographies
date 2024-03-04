2621ca5102014700516dd35cc87ab629808c1cbe
athithyaramaa1 <athithya.ramaa@kalvium.community>
2024-03-04 08:05:01 +0530

    Added a basic express app with ping route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Go to the ping route!')
+})
+
 app.get('/ping', (req, res) => {
     res.send('Hello List of Autobiographies!!')
 })
