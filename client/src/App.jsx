2a93fff1e72ef0578a07bd57706328d9d547c775
athithyaramaa1 <athithya.ramaa@kalvium.community>
2024-04-06 22:44:30 +0530

    Added a basic react app for my project, updated readme and created a landing client page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ function App() {
 
   return (
     <>
-    <h1>ASAP Project - Autobiographies</h1>
+    <h1 className="heading">ASAP Project - Autobiographies</h1>
+    <img src="https://assets.gqindia.com/photos/6465b68b24a3886180dce767/master/pass/Best-autobiographies_hp.jpeg" alt="" className="generalimg"/>
+    <p className="intro">Dive into the fascinating stories of great leaders and discover how they navigated the twists and turns of life. Get ready for a motivational boost!</p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className="countButton">
           count is {count}
         </button>
       </div>
