203823a3eddf0ec67a0c9902fb4bd3e53c09059e
athithyaramaa1 <athithya.ramaa@kalvium.community>
2024-04-12 12:43:06 +0530

    Added Update and Delete feature, designed endpoints for them.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import AddData from "./assets/AddData";
 import HomePage from "./assets/HomePage";
+import UpdateData from "./assets/UpdateData";
 import {  Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/add" element={<AddData />}></Route>
+          <Route path="/update/:index" element={<UpdateData />}></Route>
         </Routes>
       </>
     </>
