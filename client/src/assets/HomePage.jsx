0d45f9539608ecf8197f013ab6fc8c958c654eaf
athithyaramaa1 <athithya.ramaa@kalvium.community>
2024-04-15 12:42:20 +0530

    Added Logout functionality and when logout is done, the cookies are erased

--- client/src/assets/HomePage.jsx
+++ client/src/assets/HomePage.jsx
@@ -1,10 +1,26 @@
 import "../App.css";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import MultiActionAreaCard from "../assets/Card";
+
 function HomePage() {
+  const navigate = useNavigate(); // Corrected useNavigate hook
+  const Logout = () => {
+    document.cookie = "gmail=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "password=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    navigate("/");
+  };
   return (
     <>
-      <h1 className="heading"> <span className="rainbow-text">MERN</span> <span className="rainbow-text-loop">Milestone</span> - <span className="glow-text">Autobiographies</span></h1>
+      <div className="top-right">
+        <Link to="/" className="logout-link" onClick={Logout}>
+          Logout
+        </Link>
+      </div>
+      <h1 className="heading">
+        <span className="rainbow-text">MERN</span>{" "}
+        <span className="rainbow-text-loop">Milestone</span> -{" "}
+        <span className="glow-text">Autobiographies</span>
+      </h1>
       <img
         src="https://assets.gqindia.com/photos/6465b68b24a3886180dce767/master/pass/Best-autobiographies_hp.jpeg"
         alt=""
